11854cbbc5b999cc9762861faad36d4ad2e2ac2e
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-06-04 22:56:50 +0200

    Prepare to start/show search/result

--- app/src/main/java/name/juhasz/judit/udacity/googlebooks/MainActivity.java
+++ app/src/main/java/name/juhasz/judit/udacity/googlebooks/MainActivity.java
@@ -2,6 +2,8 @@ package name.juhasz.judit.udacity.googlebooks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSearch(View view) {
+        TextView searchResultTextView = (TextView) findViewById(R.id.tv_search_result);
+        searchResultTextView.setText(getString(R.string.search_result_description));
     }
 }
