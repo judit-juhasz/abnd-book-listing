8d72378a364d0a8ece2727a66e0ff4f8bf9553e3
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-06-05 23:41:04 +0200

    Finish search term based search functionality

--- app/src/main/java/name/juhasz/judit/udacity/googlebooks/MainActivity.java
+++ app/src/main/java/name/juhasz/judit/udacity/googlebooks/MainActivity.java
@@ -5,28 +5,42 @@ import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>> {
 
     private static final int BOOKS_LOADER_ID = 1;
+    private static final String SEARCH_TERM_KEY = "Search Term";
+
+    private EditText mSearchTermEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSearchTermEditText = (EditText) findViewById(R.id.et_search_term);
     }
 
     public void onClickSearch(View view) {
-        LoaderManager loaderManager = getLoaderManager();
-        loaderManager.restartLoader(BOOKS_LOADER_ID, null, this);
+        String searchTerm = mSearchTermEditText.getText().toString().trim();
+        if (!searchTerm.isEmpty()) {
+            Bundle bundle = new Bundle();
+            bundle.putString(SEARCH_TERM_KEY, searchTerm);
+            LoaderManager loaderManager = getLoaderManager();
+            loaderManager.restartLoader(BOOKS_LOADER_ID, bundle, this);
+        } else {
+            Toast.makeText(this, R.string.error_enter_search_term, Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
     public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
-        return new BookLoader(this, "ios swift");
+        String searchTerm = args.getString(SEARCH_TERM_KEY);
+        return new BookLoader(this, searchTerm);
     }
 
     @Override
